121a8f81dfa6d11a83cee731429e114fee1fd16d
Laxminath Kasam <lkasam@codeaurora.org>
2017-10-24 11:49:12 +0530

    ASoC: msm8x16-wcd: Fix lineout pop issue
    At lineout of analog codec, observe pop during powerdown.
    Handle teardown sequence to turn off registers as per
    HW sequence.
    
    Change-Id: Ic3a7dc34416b5242d15c5b4cd494b380dc7de7de
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -5156,13 +5156,8 @@ static const struct snd_soc_dapm_widget msm8x16_wcd_dapm_widgets[] = {
 
 #ifdef CONFIG_D1_ROSY
 	SND_SOC_DAPM_PGA_E("LINEOUT PA", MSM8X16_WCD_A_ANALOG_RX_LO_EN_CTL,
-			5, 1 , NULL, 0, msm8x16_wcd_codec_enable_lo_pa,
-			SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_POST_PMD),
-#else
-	SND_SOC_DAPM_PGA_E("LINEOUT PA", MSM8X16_WCD_A_ANALOG_RX_LO_EN_CTL,
 			6, 0 , NULL, 0, msm8x16_wcd_codec_enable_lo_pa,
 			SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_PRE_PMD),
-#endif
 
 	SND_SOC_DAPM_SUPPLY("VDD_SPKDRV", SND_SOC_NOPM, 0, 0,
 			    msm89xx_wcd_codec_enable_vdd_spkr,
