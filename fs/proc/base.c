882a0d89c02dcfa6de184ae9f58e029fbf4407af
Malpa73 <kmktoczek@gmail.com>
2018-12-17 17:01:55 +0100

    fix

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2887,15 +2887,12 @@ static const struct pid_entry tgid_base_stuff[] = {
 #ifdef CONFIG_CHECKPOINT_RESTORE
 	REG("timers",	  S_IRUGO, proc_timers_operations),
 #endif
-<<<<<<< HEAD
-=======
 #ifdef CONFIG_HW_DIE_CATCH
 	REG("unexpected_die_catch", S_IRUGO|S_IWUSR, proc_unexpected_die_catch_operations),
 #endif
 #ifdef CONFIG_CPU_FREQ_STAT
 	ONE("time_in_state", 0444, proc_time_in_state_show),
 #endif
->>>>>>> 88bea8d7f823... ANDROID: cpufreq_stats: add per task/uid/freq stats
 };
 
 static int proc_tgid_base_readdir(struct file *file, struct dir_context *ctx)
@@ -3275,15 +3272,12 @@ static const struct pid_entry tid_base_stuff[] = {
 	REG("projid_map", S_IRUGO|S_IWUSR, proc_projid_map_operations),
 	REG("setgroups",  S_IRUGO|S_IWUSR, proc_setgroups_operations),
 #endif
-<<<<<<< HEAD
-=======
 #ifdef CONFIG_HW_VIP_THREAD
 	REG("static_vip", S_IRUGO, proc_static_vip_operations),
 #endif
 #ifdef CONFIG_CPU_FREQ_STAT
 	ONE("time_in_state", 0444, proc_time_in_state_show),
 #endif
->>>>>>> 88bea8d7f823... ANDROID: cpufreq_stats: add per task/uid/freq stats
 };
 
 static int proc_tid_base_readdir(struct file *file, struct dir_context *ctx)
