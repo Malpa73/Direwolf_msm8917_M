eae98b1a1a56eaaa7321813846c17078bc4b0b5b
Malpa73 <kmktoczek@gmail.com>
2018-12-24 23:31:25 +0100

    fix

--- drivers/platform/msm/ipa/ipa_v2/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_v2/ipa_i.h
@@ -63,6 +63,10 @@
 
 #define IPA_MAX_STATUS_STAT_NUM 30
 
+#define IPA_IPC_LOG_PAGES 50
+
+#define IPA_MAX_NUM_REQ_CACHE 10
+
 #define IPA_TIMEOUT(value) (msecs_to_jiffies(value * 1000))
 
 #define IPADBG(fmt, args...) \
